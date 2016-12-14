71de7d2f5cbeec304cb60a6c45d07de8416230b8
Tony W <nocomme007@gmail.com>
2016-12-14 12:40:07 +0300

    create mobile menu

--- public/javascripts/index_page.js
+++ public/javascripts/index_page.js
@@ -7,10 +7,49 @@ window.onload = function() {
         document.getElementById('fimage-block').className += ' slideInLeft';
         document.getElementById('fmap-block').className += ' slideInRight';
     }
+
 }
 
-function addClass(o, c) {
-    var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g")
-    if (re.test(o.className)) return
-    o.className = (o.className + " " + c).replace(/\s+/g, " ").replace(/(^ | $)/g, "")
+var leftGroup = document.querySelectorAll('.leftA');
+var rightGroup = document.querySelectorAll('.rightA');
+var targetForFlag = document.getElementById('nav_gallery').style.display;
+var burger = document.getElementById('mobile_menu').classList;
+function flag(){
+    return targetForFlag == 'none';
 }
+function toggleAnimationClasses(){
+    burger.toggle('clicked');
+    if (flag()){
+        for(var i=0; i<2;i++)
+        {
+            leftGroup[i].classList.toggle('fadeInLeft');
+            rightGroup[i].classList.toggle('fadeInRight');
+            leftGroup[i].classList.toggle('fadeOutLeft');
+            rightGroup[i].classList.toggle('fadeOutRight');
+        }
+        targetForFlag = 'block';
+    }
+    else if(targetForFlag == 'block'){
+        for(var i=0; i<2;i++)
+        {
+            leftGroup[i].classList.toggle('fadeInLeft');
+            rightGroup[i].classList.toggle('fadeInRight');
+            leftGroup[i].classList.toggle('fadeOutLeft');
+            rightGroup[i].classList.toggle('fadeOutRight');
+        }
+        targetForFlag = 'none';
+    }
+    else{
+        for(var i=0; i<2;i++)
+        {
+            leftGroup[i].classList.toggle('fadeInLeft');
+            rightGroup[i].classList.toggle('fadeInRight');
+            leftGroup[i].classList.toggle('animated');
+            rightGroup[i].classList.toggle('animated');
+        }
+        targetForFlag = 'block';
+    }
+}
+
+mobile_menu.onclick = toggleAnimationClasses;
+close_menu.onclick = toggleAnimationClasses;
